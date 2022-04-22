556a0dcc5bd9bc2fec41e3bd3fcfef5e837ae09d
nrsimonelli <nrsimonelli@gmail.com>
2022-04-22 16:37:31 -0500

    feat(UTILS): persist rival state

---
+++ src/utils/rivalPersist.ts
@@ -0,0 +1,13 @@
+export const getInitialRival = () => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedRival = window.localStorage.getItem('stored-rival');
+    if (typeof storedRival === 'string') {
+      return storedRival;
+    }
+  }
+  return 'red';
+};
+
+export const persistCurrentRival = (rival: string) => {
+  localStorage.setItem('stored-rival', rival);
+};
